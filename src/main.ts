9f12984691fcd00254b340bc88e6ddd78c925865
Davyd Cardoso <davydkewen@gmail.com>
2024-03-13 20:18:56 -0300

    fix: adiciona correção de cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: { origin: '*', credentials: false },
+  });
+
+  await app.listen(process.env.API_PORT || 8080);
+
+  console.log(`Api running in port: ${process.env.API_PORT || 8080}`);
 }
+
 bootstrap();
